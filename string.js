eb950c0b84293a3bbc0354b81ac6b2a856b9903b
snehallsolunke <snehallimbajisolunke@gmail.com>
2023-05-13 00:36:40 +0530

    commit easily.

---
+++ string.js
@@ -0,0 +1,10 @@
+var firstName = "Snehal";
+
+console.log(firstName);
+
+console.log("Total number of characters in first name: " + firstName.length);
+
+var characterAtZeroIndex = firstName.charAt(0);
+console.log(characterAtZeroIndex);
+
+var characterAtLastIndex = firstName.charAt(firstName.length - 1);
---
+++ stringBasicAssignment01.js
@@ -0,0 +1,12 @@
+function stringBasics(){
+    console.log("My Dream Company is Capgemini");
+}
+
+var Hobby1="Fitness";
+var Hobby2="Feeding_Strays";
+var Hobby3="Listening";
+
+console.log("Hobbies are:",Hobby1,",",Hobby2,",",Hobby3,".");
+
+
+console.log("Total characters in Hobbies:",Hobby1.length+Hobby2.length+Hobby3.length);
