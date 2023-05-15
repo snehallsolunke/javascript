9d899a6f3f3e0c3c448f99f06e9aa6d312c93e87
snehallsolunke <snehallimbajisolunke@gmail.com>
2023-05-15 17:13:42 +0530

    Create 05_operatorA.js

---
+++ 05_operatorA.js
@@ -0,0 +1,25 @@
+function squareOfWordLength(string) {
+   console.log('Given Word:',string);
+   let l = string.length;
+   let square= l*l;
+   console.log(`Length of Word: ${l} Square of it's Length: ${square} `);
+   return square;
+}
+squareOfWordLength("Javascript");
+squareOfWordLength("Google Chrome");
+squareOfWordLength("Developer Smart");
+
+console.log("");
+
+function Name(){
+    string1="I am Angular Developer";
+    console.log('Given String:',string1);
+    var L=string1.length;
+    var WordsCount= string1.split(" ");
+    var N=WordsCount.length;
+    console.log(`String length:${L}, No. of words: ${N}, Result (StringLength/No. of Words):${L/N}`);
+    console.log(`String length:${L}, No. of words: ${N}, Result (StringLength*No. of words):${L*N}`);
+
+}
+ 
+Name( );
